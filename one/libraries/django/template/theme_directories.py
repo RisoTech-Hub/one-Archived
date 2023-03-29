c7381d7bff0239ab27fafe1da47eefcc71b27156
Bin Nguyễn <bin.ngoctu@gmail.com>
2023-03-29 09:37:39 +0700

    Restructure

--- one/libraries/django/template/theme_directories.py
+++ one/libraries/django/template/theme_directories.py
@@ -8,10 +8,10 @@ class Loader(BaseLoader):
         try:
             return [
                 self.engine.dirs[0] + "/ui/" + config.UI_THEME_SELECT,
-                self.engine.dirs[0] + "/admin/" + config.ADMIN_THEME_SELECT,
+                self.engine.dirs[0] + "/" + config.ADMIN_THEME_SELECT,
             ] + dirs
         except Exception:  # noqa
             return [
                 self.engine.dirs[0] + "/ui/default",
-                self.engine.dirs[0] + "/admin/adminlte",
+                self.engine.dirs[0] + "/admin",
             ] + dirs
