7507c805cbe241d5efdd30c3ffd410a4cf81991d
Bin Nguyễn <bin.ngoctu@gmail.com>
2023-04-21 22:37:23 +0700

    Signup flow

--- one/libraries/allauth/config/forms.py
+++ one/libraries/allauth/config/forms.py
@@ -1,5 +1,8 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django.core.exceptions import ValidationError
+from django.forms import BooleanField
+from django.utils.translation import gettext_lazy as _
 
 
 class UserSignupForm(SignupForm):
@@ -8,6 +11,22 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+
+    tnc = BooleanField(
+        required=True,
+        error_messages={"required": _("You must accept the terms and conditions")},
+    )
+
+    def clean_toc(self):
+        if not self.cleaned_data["tnc"]:
+            raise ValidationError(_("You must accept the terms and conditions"))
+        return self.cleaned_data["tnc"]
+
+    def save(self, request):
+        user = super().save(request)
+        user.tnc = self.cleaned_data.get("tnc")
+        user.save()
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
