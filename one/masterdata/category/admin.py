24a14f79041bd5b3576a9c27884aa86a1b95f907
Bin Nguyễn <bin.ngoctu@gmail.com>
2023-05-01 11:34:01 +0700

    Update category

--- one/masterdata/category/admin.py
+++ one/masterdata/category/admin.py
@@ -6,4 +6,11 @@ from one.masterdata.category.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(MasterModelAdmin):
-    pass
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        fieldsets[0][1]["fields"] += ("point",)
+        return fieldsets
+
+    def get_list_display(self, request):
+        list_display = super().get_list_display(request)
+        return list_display + ("point",)
