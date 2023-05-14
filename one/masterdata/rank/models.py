761fb009d5665fdbe1000fcde0e6f5577d44948a
Bin Nguyen <bin.ngoctu@gmail.com>
2023-05-14 12:43:14 +0700

    update masterdata

--- one/masterdata/rank/models.py
+++ one/masterdata/rank/models.py
@@ -1,3 +1,4 @@
+from django.db.models import FloatField
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
@@ -5,6 +6,8 @@ from one.libraries.utils.models import MasterModel, UserStampedModel
 
 
 class Rank(MasterModel, TimeStampedModel, UserStampedModel):
+    discount = FloatField(_("Discount"), default=0)
+
     class Meta:
         verbose_name = _("Rank")
         verbose_name_plural = _("Ranks")
