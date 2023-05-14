ef019478424fff8664482f8f34ba2a05c35b341f
Bin Nguyen <bin.ngoctu@gmail.com>
2023-05-14 12:33:01 +0700

    order

--- one/order/models.py
+++ one/order/models.py
@@ -1,12 +1,15 @@
 from django.utils.translation import gettext_lazy as _
-from django.db.models import CASCADE, ForeignKey, Model
+from django.db.models import CASCADE, ForeignKey, Model, IntegerField
 from model_utils.models import TimeStampedModel
 
 from one.libraries.utils.models import UserStampedModel
+from one.masterdata.category.models import Category
+from one.masterdata.sku.models import SKU
+from one.masterdata.uom.models import UOM
 
 
 class Order(TimeStampedModel, UserStampedModel):
-    customer = ForeignKey("customer.Customer", verbose_name=_("Customer"), on_delete=CASCADE)
+    customer = ForeignKey("customer.Customer", verbose_name=_("Customer"), on_delete=CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name = _("Order")
@@ -16,6 +19,12 @@ class Order(TimeStampedModel, UserStampedModel):
 
 class OrderLine(Model):
     order = ForeignKey(Order, verbose_name=_("Order"), on_delete=CASCADE, related_name="order_line")
+
+    category = ForeignKey(Category, verbose_name=_("Category"), on_delete=CASCADE)
+    sku = ForeignKey(SKU, verbose_name=_("SKU"), on_delete=CASCADE)
+    oum = ForeignKey(UOM, verbose_name=_("OUM"), on_delete=CASCADE)
+
+    quantity = IntegerField(_("Quantity"), default=0)
 
     class Meta:
         verbose_name = _("Order Line")
