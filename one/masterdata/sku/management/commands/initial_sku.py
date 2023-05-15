61cf8efa4a9e9632c32a1b73e41ec9272095c1c9
Bin Nguyen <bin.ngoctu@gmail.com>
2023-05-16 01:14:53 +0700

    update masterdata

--- one/masterdata/sku/management/commands/initial_sku.py
+++ one/masterdata/sku/management/commands/initial_sku.py
@@ -14,8 +14,10 @@ class Command(BaseCommand):
             {"code": "SKU_POST_AOC", "name": "AOC Post", "uom": "UOM_WORD", "markup": 1},
             {"code": "SKU_POST_ADS", "name": "Ads Post", "uom": "UOM_WORD", "markup": 1},
             {"code": "SKU_POST_SEO", "name": "Bài SEO", "uom": "UOM_WORD", "markup": 1},
+
             {"code": "SKU_VISUAL_VIDEO", "name": "Video(Visual)", "uom": "UOM_SECOND", "markup": 1},
             {"code": "SKU_SCRIPT", "name": "Kịch bản", "uom": "UOM_SECOND", "markup": 1},
+
             {"code": "SKU_VISUAL_SINGLE_PHOTO", "name": "Single Photo(Visual)", "uom": "UOM_PIECE", "markup": 1},
             {"code": "SKU_VISUAL_CAROUSEL", "name": "Multi / Carousel(Visual)", "uom": "UOM_PIECE", "markup": 1},
             {"code": "SKU_DIRECTION_WEEKLY", "name": "Weekly Content Direction", "uom": "UOM_PIECE", "markup": 1},
@@ -23,6 +25,7 @@ class Command(BaseCommand):
             {"code": "SKU_PLAN_CONTENT", "name": "Content Plan", "uom": "UOM_PIECE", "markup": 1},
             {"code": "SKU_PLAN_CREATIVE", "name": "Creative Plan", "uom": "UOM_PIECE", "markup": 1},
             {"code": "SKU_PLAN_DIGITAL", "name": "Digital Plan", "uom": "UOM_PIECE", "markup": 1},
+
             {"code": "SKU_TRAINING_COPYWRITING", "name": "Training Copywriting", "uom": "UOM_SECTION", "markup": 1},
             {"code": "SKU_TRAINING_PLANNING", "name": "Training Creative Planning", "uom": "UOM_SECTION", "markup": 1},
         ],
@@ -41,6 +44,6 @@ class Command(BaseCommand):
                 code=sku["code"],
                 name=sku["name"],
                 base_uom=UOM.objects.filter(code=sku["uom"]).first(),
-                markup=sku["markup"],
+                defaults={"point": sku.get("point", 1), "markup": sku.get("markup", 1)}
             )
         self.stdout.write(self.style.SUCCESS("Successfully created SKU objects."))
