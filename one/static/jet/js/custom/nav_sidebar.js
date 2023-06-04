e38767e3a8450863128799146efc3ba46dfe2602
Ly Quang Thai <thai.ly@masterisehomes.com>
2023-06-04 22:02:04 +0700

    update search page in dropdown

--- one/static/jet/js/custom/nav_sidebar.js
+++ one/static/jet/js/custom/nav_sidebar.js
@@ -3,19 +3,23 @@
   const toggleNavSidebar = document.getElementById('toggle-nav-sidebar');
   if (toggleNavSidebar !== null) {
     const navLinks = document.querySelectorAll('#nav-sidebar a');
+
     function disableNavLinkTabbing() {
       for (const navLink of navLinks) {
         navLink.tabIndex = -1;
       }
     }
+
     function enableNavLinkTabbing() {
       for (const navLink of navLinks) {
         navLink.tabIndex = 0;
       }
     }
+
     function disableNavFilterTabbing() {
       document.getElementById('nav-filter').tabIndex = -1;
     }
+
     function enableNavFilterTabbing() {
       document.getElementById('nav-filter').tabIndex = 0;
     }
@@ -54,9 +58,27 @@
     if (!navSidebar) {
       return;
     }
-    navSidebar.querySelectorAll('th[scope=row] a').forEach((container) => {
-      options.push({ title: container.innerHTML, node: container });
-    });
+
+    /* OLD code
+        navSidebar.querySelectorAll('th[scope=row] a').forEach((container) => {
+          options.push({ title: container.innerHTML, node: container });
+        });
+         */
+
+    $('#div_result').empty();
+    navSidebar
+      .querySelectorAll('div[data-kt-menu-flip] .menu-sub .menu-item a')
+      .forEach((container) => {
+        options.push({ title: container.title, node: container });
+        // console.log('---', container)
+        const clonedE = $(container)
+          .clone()
+          .addClass('d-flex text-hover-primary align-items-center mb-5');
+        clonedE.find('.menu-bullet').remove();
+        clonedE.appendTo('#div_result');
+      });
+
+    // console.log('options', options);
 
     function checkValue(event) {
       let filterValue = event.target.value;
@@ -79,12 +101,41 @@
         }
         // show/hide parent <TR>
         o.node.parentNode.parentNode.style.display = displayValue;
+        // console.log('displayValue', displayValue, 'o.node.href', o.node, '#div_result a[href="' + $(o.node).attr('href') + '"]')
+        if (displayValue === 'none') {
+          $('#div_result a[href="' + $(o.node).attr('href') + '"]')
+            .removeClass('d-flex')
+            .addClass('d-none');
+        } else {
+          $('#div_result a[href="' + $(o.node).attr('href') + '"]')
+            .removeClass('d-none')
+            .addClass('d-flex');
+        }
       }
+      console.log('filterValue', filterValue, 'matches', matches);
+      const $result = $('#div_result');
+      const $recently = $('#div_recently');
+      const $no_result = $('#div_no_result');
+
       if (!filterValue || matches) {
         event.target.classList.remove('no-results');
+        $result.removeClass('d-none');
+        $recently.addClass('d-none');
+        $no_result.addClass('d-none');
       } else {
         event.target.classList.add('no-results');
+        $result.addClass('d-none');
+        if (!matches) {
+          $recently.addClass('d-none');
+          $no_result.removeClass('d-none');
+        } else {
+          if (filterValue === '') {
+            $recently.removeClass('d-none');
+            $no_result.addClass('d-none');
+          }
+        }
       }
+
       sessionStorage.setItem('django.admin.navSidebarFilterValue', filterValue);
     }
 
@@ -101,6 +152,7 @@
       checkValue({ target: nav, key: '' });
     }
   }
+
   window.initSidebarQuickFilter = initSidebarQuickFilter;
   initSidebarQuickFilter();
 }
